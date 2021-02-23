9455fd2eedbcda0f5d826646a3cf7870598e8599
Hernan <hlopez@hsoft.ws>
2021-02-23 16:32:12 -0400

    Fix Force

--- libs/core/force.ts
+++ libs/core/force.ts
@@ -20,8 +20,8 @@ namespace Force {
         private readonly rangefactor = 20 / 3.3;
         private readonly Vadc_3 = 3.3 / 4096;
 
-        private myBoardID: number;
-        private myClickID: number;
+        private myBoardID: BoardID;
+        private myClickID: ClickID;
 
         constructor(boardID: BoardID, clickID: ClickID) {
             this.myBoardID = boardID;
@@ -42,18 +42,16 @@ namespace Force {
         //% weight=90 blockGap=12 color=#9E4894 icon=""
 
         forceclick(): number {
-            let A: number
-            let sumA: number
-            let Force_voltage: number
-            let Force_val: number
-            
+            let sample: number
+            let sum_sample: number
+            let Force_val: number 
+            sum_sample = 0          
             for (let i = 1; i <= 20; i++) {
-                A = (bBoard_Control.analogRead(clickADCPin.AN, this.myBoardID, this.myClickID))
-                sumA += A;
+                sample = (bBoard_Control.analogRead(clickADCPin.AN, this.myBoardID, this.myClickID))
+                sum_sample += sample;
             }
-            sumA = sumA / 20;
-            Force_voltage = sumA * this.Vadc_3; //Voltage for the force click board
-            Force_val = Force_voltage * this.rangefactor;
+            sum_sample = sum_sample / 20
+            Force_val = Math.roundWithPrecision(sum_sample * this.Vadc_3 * this.rangefactor, 2); //Voltage for the force click board
             return Force_val
         }
     }
