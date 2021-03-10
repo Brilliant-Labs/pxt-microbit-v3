e87840f7d5f1aa95fb30b55e46d9d68f7f5e602f
Hernan <hlopez@hsoft.ws>
2021-03-10 16:19:20 -0400

    Add Stepper_5

---
+++ libs/core/Stepper_5.ts
@@ -0,0 +1,41 @@
+//-------------------------Click Board Stepper_5 -----------------------------------
+//% weight=100 color=#EF697B icon=""
+//% advanced=true
+//% labelLineWidth=1005
+namespace Stepper_5 {
+    export enum Rotation {
+        //% block="Forward"
+        Forward = 0,
+        //% block="Backwards"
+        Backwards = 1
+    }
+
+
+
+
+
+    
+    //% blockId=step
+    //% block="Single step motor %direction on click%clickBoardNum"
+    //% weight=100
+    //% blockGap=7
+    export function step(direction: Stepper_5.Rotation, clickBoardNum: clickBoardID): void {
+        bBoard.setPin(clickIOPin.PWM, clickBoardNum)
+        basic.pause(1)
+        bBoard.clearPin(clickIOPin.PWM, clickBoardNum)
+    }
+
+    //% blockId=stepNum
+    //% block="Step motor %numSteps times %direction on click%clickBoardNum"
+    //% weight=100
+    //% blockGap=7
+    export function stepNumber(numSteps: number, direction: Stepper_5.Rotation, clickBoardNum: clickBoardID): void {
+        bBoard.writePin(direction, clickIOPin.RST, clickBoardNum)
+        for (let i = 0; i < numSteps; i++) {
+            bBoard.setPin(clickIOPin.PWM, clickBoardNum)
+            control.waitMicros(500);
+            bBoard.clearPin(clickIOPin.PWM, clickBoardNum)
+            control.waitMicros(500);
+        }
+    }
+}
