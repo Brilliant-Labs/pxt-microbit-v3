32ae36775ee5aa8b21c458ec765eea211c28e384
Hernan <hernan@brilliantlabs.ca>
2021-06-02 13:26:17 -0300

    Add Temp_Log_2

--- libs/core/Temp_Log_2.ts
+++ libs/core/Temp_Log_2.ts
@@ -3,12 +3,12 @@
 //% advanced=true
 //% labelLineWidth=1002
 namespace Temp_Log_2 {
-    export enum TempUnits {
-        //% block="C"
-        C = 0,
-        //% block="F"
-        F = 1
-    }
+    declare const TMP116_REG_TEMP = 0x00;
+    declare const TMP116_REG_CONFIG = 0x01;
+    declare const TMP116_REG_HIGH_LIMIT = 0x02;
+    declare const TMP116_REG_LOW_LIMIT= 0x03; 
+    declare const TMP116_REG_DEVICE_ID = 0x0F;
+    declare const TMP116_DEVICE_ADDRESS = 0x48; 
 
     /**
      * Sets Temp_Log_2 Click object.
@@ -24,28 +24,28 @@ namespace Temp_Log_2 {
     }
 
     export class Temp_Log_2 {
-        private readonly DEFAULT_I2C_ADDRESS = 0x48;
-        private readonly TMP116_REG_TEMP = 0x00;
-        private readonly TMP116_REG_CONFIG = 0x01;
-        private readonly TMP116_REG_HIGH_LIMIT = 0x02;
-        private readonly TMP116_REG_LOW_LIMIT = 0x03;
-        private readonly TMP116_REG_DEVICE_ID = 0x0F;
-
-        private myBoardID: number
-        private myClickID: number
-
+        private readonly DEFAULT_I2C_ADDRESS = 0x48 
+        private myI2CAddress:number 
+        private myBoardID: number 
+        private myClickID: number 
+        
         constructor(boardID: BoardID, clickID: ClickID) {
             this.myBoardID = boardID;
             this.myClickID = clickID;
-            this.initialize()
+            this.initialize();
         }
 
         initialize() {
-            this.writeTMP116(this.TMP116_REG_CONFIG, 0x0220) //Initialize the Config register
+            this.myI2CAddress = this.DEFAULT_I2C_ADDRESS 
+            this.writeTMP116(TMP116_REG_CONFIG,0x0220) //Initialize the Config register 
         }
 
         readTMP116Reg(register: number): number {
-            return this.readTMP116(this.TMP116_REG_CONFIG)
+            return this.readTMP116(TMP116_REG_CONFIG)
+        }
+
+        readT():number { 
+            return this.readTemperatureC();
         }
 
         writeTMP116(register: number, value: number) {
@@ -59,48 +59,52 @@ namespace Temp_Log_2 {
         //Reads two consecutive bytes from a given location
         //Stores the result at the provided outputPointer
         readTMP116(register: number): number {
-            let tempBuf = pins.createBuffer(2)
-            tempBuf.setNumber(NumberFormat.UInt8LE, 0, this.DEFAULT_I2C_ADDRESS)
-            tempBuf.setNumber(NumberFormat.UInt8LE, 2, register)
-            bBoard_Control.BLiX(this.myBoardID, this.myClickID, 0, I2C_module_id, I2C_WRITE_id, null, tempBuf, 0)
-
-            let i2cBuffer = pins.createBuffer(2);
-            i2cBuffer = bBoard_Control.BLiX(this.myBoardID, this.myClickID, 0, I2C_module_id, I2C_READ_NO_MEM_id, [this.DEFAULT_I2C_ADDRESS, 2], null, 2)
-            let sReturn = Math.roundWithPrecision(i2cBuffer.getNumber(NumberFormat.Int16BE, 0), 1)
+            let i2cBuffer = pins.createBuffer(2); 
+            bBoard_Control.i2cWriteNumber(this.myI2CAddress,register,NumberFormat.Int8LE,true,this.myBoardID,this.myClickID) 
+            i2cBuffer = bBoard_Control.I2CreadNoMem(this.myI2CAddress,2,this.myBoardID,this.myClickID);
+            let sReturn = Math.roundWithPrecision(i2cBuffer.getNumber(NumberFormat.Int16BE,0),1)
+            // let msb = i2cBuffer.getUint8(0)
+            // let lsb = i2cBuffer.getUint8(1)
+            //return (msb << 8 | lsb)
             return sReturn
         }
 
-        //%blockId=Temp_Log_2_Temp
-        //%block="$this Temperature in $units"
+        //%blockId=Temp_Log_readTemperatureC
+        //%block="$this Get temperature in Celcius"
         //% blockGap=7
         //% advanced=false
-        //% blockNamespace=Temp_Log_2
-        //% this.shadow=variables_get
         //% this.defl="Temp_Log_2"
-        Temp(units: Temp_Log_2.TempUnits): number {
-            let tempC = (this.readTMP116(this.TMP116_REG_TEMP) * 0.0078125)
-            let tempF = tempC * 9.0 / 5.0 + 32.0;
-            return units == TempUnits.C ? tempC : tempF
+        readTemperatureC(): number {
+            return (this.readTMP116(TMP116_REG_TEMP) * 0.0078125)
+        }
+
+        //%blockId=Temp_Log_readTemperatureF
+        //%block="$this Get temperature in Fahrenheit"
+        //% blockGap=7
+        //% advanced=false
+        //% this.defl="Temp_Log_2"
+        readTemperatureF(): number { 
+            return ((this.readTemperatureC()) * 9.0 / 5.0 + 32.0) 
         }
 
         readHighLimit(): number {
-            return (this.readTMP116(this.TMP116_REG_HIGH_LIMIT) * 0.0078125)
+            return (this.readTMP116(TMP116_REG_HIGH_LIMIT) * 0.0078125)
         }
 
         readLowLimit(): number {
-            return (this.readTMP116(this.TMP116_REG_LOW_LIMIT) * 0.0078125)
+            return (this.readTMP116(TMP116_REG_LOW_LIMIT) * 0.0078125)
         }
 
         writeHighLimit(limit: number) {
-            this.writeTMP116(this.TMP116_REG_HIGH_LIMIT, (limit / 0.0078125))
+            this.writeTMP116(TMP116_REG_HIGH_LIMIT, (limit / 0.0078125))
         }
 
         writeLowLimit(limit: number) {
-            this.writeTMP116(this.TMP116_REG_LOW_LIMIT, (limit / 0.0078125))
+            this.writeTMP116(TMP116_REG_LOW_LIMIT, (limit / 0.0078125))
         }
 
         readDeviceId(): number {
-            return (this.readTMP116(this.TMP116_REG_DEVICE_ID))
+            return (this.readTMP116(TMP116_REG_DEVICE_ID))
         }
     }
 }
