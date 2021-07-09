066b7321cd6ed9e0a977a5b4fdae5682bea90790
Hernan <hlopez@hsoft.ws>
2021-07-09 11:34:49 -0300

    Add Stepper_5

--- libs/core/Stepper_5.ts
+++ libs/core/Stepper_5.ts
@@ -10,32 +10,51 @@ namespace Stepper_5 {
         Backwards = 1
     }
 
-
-
-
-
-    
-    //% blockId=step
-    //% block="Single step motor %direction on click%clickBoardNum"
-    //% weight=100
-    //% blockGap=7
-    export function step(direction: Stepper_5.Rotation, clickBoardNum: clickBoardID): void {
-        bBoard.setPin(clickIOPin.PWM, clickBoardNum)
-        basic.pause(1)
-        bBoard.clearPin(clickIOPin.PWM, clickBoardNum)
+    /**
+     * Sets Stepper object.
+     * @param boardID the boardID
+     * @param clickID the ClickID
+     * @param Stepper the Stepper Object
+     */
+    //% block="$boardID $clickID"
+    //% advanced=false
+    //% $boardID.shadow="BoardID.zero"
+    //% blockSetVariable="Stepper"
+    //% weight=110
+    export function createStepper(boardID: BoardID, clickID: ClickID): Stepper {
+        return new Stepper(boardID, clickID);
     }
+    export class Stepper {
+        private myBoardID: BoardID
+        private myClickID: ClickID
 
-    //% blockId=stepNum
-    //% block="Step motor %numSteps times %direction on click%clickBoardNum"
-    //% weight=100
-    //% blockGap=7
-    export function stepNumber(numSteps: number, direction: Stepper_5.Rotation, clickBoardNum: clickBoardID): void {
-        bBoard.writePin(direction, clickIOPin.RST, clickBoardNum)
-        for (let i = 0; i < numSteps; i++) {
-            bBoard.setPin(clickIOPin.PWM, clickBoardNum)
-            control.waitMicros(500);
-            bBoard.clearPin(clickIOPin.PWM, clickBoardNum)
-            control.waitMicros(500);
+        constructor(boardID: BoardID, clickID: ClickID) {
+            this.myBoardID = boardID;
+            this.myClickID = clickID;
+        }
+
+        //% blockId=step
+        //% block="Single step motor %direction on click%clickBoardNum"
+        //% weight=100
+        //% blockGap=7
+        step(direction: Stepper_5.Rotation): void {
+            bBoard_Control.setPin(clickIOPin.PWM, this.myBoardID, this.myClickID)
+            basic.pause(1)
+            bBoard_Control.clearPin(clickIOPin.PWM, this.myBoardID, this.myClickID)
+        }
+
+        //% blockId=stepNum
+        //% block="Step motor %numSteps times %direction on click%clickBoardNum"
+        //% weight=100
+        //% blockGap=7
+        stepNumber(numSteps: number, direction: Stepper_5.Rotation): void {
+            bBoard_Control.writePin(direction, clickIOPin.RST, this.myBoardID, this.myClickID)
+            for (let i = 0; i < numSteps; i++) {
+                bBoard_Control.setPin(clickIOPin.PWM, this.myBoardID, this.myClickID)
+                control.waitMicros(500);
+                bBoard_Control.clearPin(clickIOPin.PWM, this.myBoardID, this.myClickID)
+                control.waitMicros(500);
+            }
         }
     }
 }
