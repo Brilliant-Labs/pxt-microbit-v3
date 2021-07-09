43605a8c26d3fbec54eef6c4a7c1bd37edf1154a
Hernan <hlopez@hsoft.ws>
2021-07-09 11:34:48 -0300

    Fix Proximity_2

--- libs/core/Proximity_2.ts
+++ libs/core/Proximity_2.ts
@@ -1,11 +1,10 @@
-
 //-------------------------Click Board Proximity_2 -----------------------------------
 //% weight=904 color=#33BEBB icon="↦"
 //% advanced=true
 //% labelLineWidth=1002
 namespace Proximity_2 {
     enum proximity_2_Interrupts { ALS_INT, PROX_INT, NO_INT };
-
+​
     /**
      * Sets Proximity_2 Click object.
      * @param boardID the boardID
@@ -18,9 +17,9 @@ namespace Proximity_2 {
     export function createProximity_2(boardID: BoardID, clickID: ClickID): Proximity_2 {
         return new Proximity_2(boardID, clickID);
     }
-
+​
     export class Proximity_2 {
-        private readonly DEFAULT_I2C_ADDRESS = 0x94
+        private readonly DEFAULT_I2C_ADDRESS = 0x4A
         private readonly INTERRUPT_STATUS = 0x00
         private readonly MAIN_CONFIGURATION = 0x01
         private readonly RECEIVE_CONFIGURATION = 0x02
@@ -37,34 +36,35 @@ namespace Proximity_2 {
         private readonly PROX_THRESHOLD = 0x0C
         private readonly DIGITAL_GAIN_TRIM_GREEN = 0x0F
         private readonly DIGITAL_GAIN_TRIM_INFRARED = 0x10
-
+​
         private myBoardID: number
         private myClickID: number
-
+        private myI2CAddress:number
+​
         constructor(boardID: BoardID, clickID: ClickID) {
             this.myBoardID = boardID;
             this.myClickID = clickID;
             this.proximity_2_Initialize()
         }
-
+​
         // Setup the chip for proximity sensing
         proximity_2_Initialize() {
             //this.isInitialized[this.myBoardID] = 1;
+            this.myI2CAddress = this.DEFAULT_I2C_ADDRESS
             this.Write_proximity_2_Register(this.MAIN_CONFIGURATION, 0b110000);
             this.Write_proximity_2_Register(this.PROX_THRESHOLD_INDICATOR, 0b01000000);
             this.Write_proximity_2_Register(this.PROX_THRESHOLD_INDICATOR, 0b01000000);
             this.Write_proximity_2_Register(this.TRANSMIT_CONFIGURATION, 0b00001111);
         }
-
+​
         // Write byte 'byte' to register 'reg'
         Write_proximity_2_Register(register: number, value: number) {
-            let i2cBuffer = pins.createBuffer(3)
-            i2cBuffer.setNumber(NumberFormat.UInt8LE, 0, this.DEFAULT_I2C_ADDRESS)
-            i2cBuffer.setNumber(NumberFormat.UInt8LE, 1, register)
-            i2cBuffer.setNumber(NumberFormat.UInt8LE, 2, value)
-            bBoard_Control.BLiX(this.myBoardID, this.myClickID, 0, I2C_module_id, I2C_WRITE_id, null, i2cBuffer, 0)
+            let i2cBuffer = pins.createBuffer(2)
+            i2cBuffer.setNumber(NumberFormat.UInt8LE, 0, register)
+            i2cBuffer.setNumber(NumberFormat.UInt8LE, 1, value)
+            bBoard_Control.i2cWriteBuffer(this.myI2CAddress,i2cBuffer,this.myBoardID,this.myClickID)
         }
-
+​
         //%blockId=proximity_2_ReadProximity
         //%block="$this proximity"
         //% blockGap=7
@@ -77,7 +77,7 @@ namespace Proximity_2 {
             let val = this.Read_proximity_2_Register(this.ADC_BYTE_PROX);
             return val;
         }
-
+​
         //%blockId=proximity_2_ReadALS
         //%block="$this illuminance(lux)"
         //% blockGap=7
@@ -89,19 +89,15 @@ namespace Proximity_2 {
             let val = (this.Read_proximity_2_Register(this.ADC_HIGH_ALS) << 8) | this.Read_proximity_2_Register(this.ADC_LOW_ALS);
             return val * 0x03125; //Assumption that ALSPGA == 0. 0.03125 Lux per LSB
         }
-
+​
         // Read a byte from register 'reg'
         Read_proximity_2_Register(register: number): number {
-            let tempBuf = pins.createBuffer(2);
-            tempBuf.setNumber(NumberFormat.UInt8LE, 0, this.DEFAULT_I2C_ADDRESS)
-            tempBuf.setNumber(NumberFormat.UInt8LE, 1, register)
-            bBoard_Control.BLiX(this.myBoardID, this.myClickID, 0, I2C_module_id, I2C_WRITE_id, null, tempBuf, 0)
-
-            let i2cBuffer = pins.createBuffer(1);
-            i2cBuffer = bBoard_Control.BLiX(this.myBoardID, this.myClickID, 0, I2C_module_id, I2C_READ_NO_MEM_id, [this.DEFAULT_I2C_ADDRESS, 1], null, 1)
+            let i2cBuffer = pins.createBuffer(2);
+            bBoard_Control.i2cWriteNumber(this.myI2CAddress, register, NumberFormat.Int8LE, true, this.myBoardID, this.myClickID)
+            i2cBuffer = bBoard_Control.I2CreadNoMem(this.myI2CAddress, 1, this.myBoardID, this.myClickID);
             return i2cBuffer.getUint8(0)
         }
-
+​
         proximity_2_Read_Interrupt(): number {
             let val = this.Read_proximity_2_Register(this.INTERRUPT_STATUS);
             if (val & 0b1) {
@@ -114,16 +110,16 @@ namespace Proximity_2 {
                 return proximity_2_Interrupts.NO_INT;
             }
         }
-
+​
         proximity_2_Set_Threshold(thresh: number) {
             this.Write_proximity_2_Register(this.PROX_THRESHOLD, thresh);
         }
-
+​
         proximity_2_Set_Als_Upper_Threshold(thresh: number) {
             this.Write_proximity_2_Register(this.ALS_UPPER_THRESHOLD_HIGH, thresh >> 8);;
             this.Write_proximity_2_Register(this.ALS_UPPER_THRESHOLD_LOW, thresh & 0xFF);
         }
-
+​
         proximity_2_Set_Als_Lower_Threshold(thresh: number) {
             this.Write_proximity_2_Register(this.ALS_LOWER_THRESHOLD_HIGH, thresh >> 8);
             this.Write_proximity_2_Register(this.ALS_LOWER_THRESHOLD_LOW, thresh & 0xFF);
